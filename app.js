8e43f9ceb28c9fa5bfb7e516a1a932851c1e32de
Omar <124088826+oali220@users.noreply.github.com>
2023-03-10 17:43:52 -0600

    connected api data to js and html

--- app.js
+++ app.js
@@ -13,9 +13,16 @@ let weather = {
   displayWeather: function(data) {
     let { name } = data;
     const { icon } = data.weather[0];
-    const{ temp_min, temp_max } = data.main;
+    const{ temp, temp_min, temp_max, humidity } = data.main;
     const { speed } = data.wind;
     console.log(name, icon, temp_min, temp_max, speed);
+    document.querySelector(".cityname").innerText = name;
+    document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".temp-min").innerText = temp_min;
+    document.querySelector(".temp-max").innerText = temp_max;
+    document.querySelector(".humidity").innerText = humidity;
+    document.querySelector(".wind").innerText = speed;
+    document.querySelector(".current-weather").innerText = temp;
   }
 
 }
